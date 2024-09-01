002d8530cab89d0d8568e3b75a56d0c5fa231e00
vivekpalan <venomcodes0531@gmail.com>
2024-09-01 17:26:40 +0530

    Minor changes

--- src/components/CreateBlogs.jsx
+++ src/components/CreateBlogs.jsx
@@ -13,36 +13,53 @@ const CreateBlogs = () => {
 
     function handleSubmit(e){
         e.preventDefault();
-
         const data = {text,content,author,date,month,category,image}
         fetch('http://localhost:8005/blogs',{
             method: "POST",
             content: "/application.json",
             body: JSON.stringify(data)
         })
-        history.pushState("/")
-        
+        alert('Blog successfully added')
+        history.go(-1)      
     }
     return(
         <>
             <div className="create-blogs">
                 <form action="#">
+                    <h3 style={{textAlign:"center"}}>Enter your blog</h3>
                     <input type="text" name="text" id="text" required value={text} placeholder="Enter Blog title" onChange={e => setText(e.target.value)}/>
-                    <textarea name="blog-content" id="blog-content" required placeholder="Enter the blog content" value={content}  onChange={e => setContent(e.target.value)}></textarea>
                     <input type="text" name="author" id="author" required placeholder="Enter Blog author" value={author}  onChange={e => setAuthor(e.target.value)}/>
                     <input type="text" name="" id="" placeholder="Enter date" required value={date}  onChange={e => setDate(e.target.value)}/>
-                    <input type="text" name="" id="" placeholder="Enter month" required value={month}  onChange={e => setMonth(e.target.value)} />
 
-                    <select name="category" required id="category" value={category}  onChange={e => setCategory(e.target.value)}>
+                    <select  style={{marginTop:"10px",padding:"10px 20px"}} name="month" required id="month" value={month} onChange={e => setMonth(e.target.value)}>
+
+                        <option value="Jan">Jan</option>
+                        <option value="Feb">Feb</option>
+                        <option value="Mar">Mar</option>
+                        <option value="Apr">Apr</option>
+                        <option value="May">May</option>
+                        <option value="Jun">Jun</option>
+                        <option value="Jul">Jul</option>
+                        <option value="Aug">Aug</option>
+                        <option value="Sept">Sept</option>
+                        <option value="Oct">Oct</option>
+                        <option value="Nov">Nov</option>
+                        <option value="Dec">Dec</option>
+                    </select>
+
+
+
+                    <select style={{marginTop:"10px",padding:"10px 20px"}} name="category" required id="category" value={category}  onChange={e => setCategory(e.target.value)}>
                         <option value="Tech">Tech</option>
                         <option value="Finance">Finance</option>
                         <option value="Mindfulness">Mindfulness</option>
                         <option value="Learning">Learning</option>
                         <option value="Fitness">Fitness</option>
                     </select>
-                    <input type="file" name="Upload Image" id="image" required value={image}  onChange={e => setImage(e.target.value)}/>
+                    <input type="url" name="Upload Image" id="image" required value={image} placeholder="Enter Image URL"  onChange={e => setImage(e.target.value)}/>
 
-                    <input type="submit" value="Add" onClick={(e) => handleSubmit(e)} />
+                    <textarea name="blog-content" id="blog-content" required placeholder="Enter the blog content" value={content}  onChange={e => setContent(e.target.value)}></textarea>
+                    <input className="submit-btn" type="submit" value="Add" onClick={(e) => handleSubmit(e)} />
                 </form>
             </div>
 
